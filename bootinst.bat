ecd599916df4d4eaf6db40cfb121250d6a260539
semsv <semen_s84@mail.ru>
2019-05-16 18:07:56 +0000

    Add files via upload

--- bootinst.bat
+++ bootinst.bat
@@ -5,24 +5,15 @@ exec /bin/sh "$(dirname "$0")"/bootinst.sh
 @echo off
 COLOR 2F
 cls
-echo ===============================================================================
-echo.
-echo                            ________.__
-echo                           /   ____/^|  ^| _____  ___  ___
-echo                           \____  \ ^|  ^| \__  \ \  \/  /
-echo                           /       \^|  ^|__/ __ \_^>    ^<
-echo                          /______  /^|____(____  /__/\_ \
-echo                                 \/           \/      \/
-echo.
-echo ===============================================================================
+echo Start Alesya Linux Installer...[OK]
 echo.
 
 set DISK=none
 set BOOTFLAG=boot666s.tmp
 
 :checkPrivileges
-mkdir "%windir%\SlaxAdminCheck" 2>nul
-if '%errorlevel%' == '0' rmdir "%windir%\SlaxAdminCheck" & goto gotPrivileges else goto getPrivileges
+mkdir "%windir%\ALSYAdminCheck" 2>nul
+if '%errorlevel%' == '0' rmdir "%windir%\ALSYAdminCheck" & goto gotPrivileges else goto getPrivileges
 
 :getPrivileges
 ECHO.
@@ -53,12 +44,12 @@ if %OS% == Windows_NT goto setupNT
 goto setup95
 
 :setupNT
-\boot\syslinux.exe -maf -d /boot/ %DISK%:
+\Alesya_Linux\boot\syslinux.exe -maf -d /Alesya_Linux/boot/ %DISK%:
 if %ERRORLEVEL% == 0 goto setupDone
 goto errorFound
 
 :setup95
-\boot\syslinux.com -maf -d /boot/ %DISK%:
+\Alesya_Linux\boot\syslinux.com -maf -d /Alesya_Linux/boot/ %DISK%:
 if %ERRORLEVEL% == 0 goto setupDone
 goto errorFound
 
@@ -75,15 +66,15 @@ goto pauseit
 :refuseDisk
 color 4F
 echo.
-echo Directory %DISK%:\slax\boot\ seems to be on the same physical disk as your Windows.
+echo Directory %DISK%:\Alesya_Linux\boot\ seems to be on the same physical disk as your Windows.
 echo Installing bootloader would harm your Windows and thus is disabled.
-echo Please put Slax to a different drive and try again.
+echo Please put AlsyLinux to a different drive and try again.
 goto pauseit
 
 :readOnly
 color 4F
 echo.
-echo You're starting Slax installer from a read-only media, this will not work.
+echo You're starting AlsyLinux installer from a read-only media, this will not work.
 goto pauseit
 
 :DiskNotFound
