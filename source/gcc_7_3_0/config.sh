c16ad7e2399d7adf276202f8f5625b7ac91e3fcb
semsv <semen_s84@mail.ru>
2019-08-09 17:00:40 +0500

    Update config.sh

--- source/gcc_7_3_0/config.sh
+++ source/gcc_7_3_0/config.sh
@@ -1,10 +1,15 @@
+#/bin/sh
+ALSYBUILDPATH="/src-linux/gcc-7.3.0/build"
+echo $ALSYBUILDPATH > "ALSYBUILDPATH"
 case $(uname -m) in
   x86_64)
     sed -e '/m64=/s/lib64/lib/' \
         -i.orig gcc/config/i386/t-linux64
   ;;
 esac
-
+if [ -d build ]; then
+ rm -rd build
+fi
 mkdir build                                          &&
 cd    build                                          &&
 
