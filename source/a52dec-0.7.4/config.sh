c10b100274fb0f2b4bd2d9b95fb6b3b203366de7
semsv <semen_s84@mail.ru>
2019-08-28 17:37:59 +0000

    Wed 28 Aug 2019 05:37:59 PM UTC

--- source/a52dec-0.7.4/config.sh
+++ source/a52dec-0.7.4/config.sh
@@ -1,3 +1,5 @@
-CXXFLAGS+="-O2 -fPIC" \
-./configure --prefix=/usr/src/tools/a52dec-0.7.4 \
-            --enable-shared
+   ./configure --prefix=/usr/src/tools/a52dec-0.7.4 \
+               --mandir=/usr/share/man \
+               --enable-shared \
+               --disable-static \
+               CFLAGS="-g -O2 $([ $(uname -m) = x86_64 ] && echo -fPIC)" && make
