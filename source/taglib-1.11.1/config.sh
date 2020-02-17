498993a4c47308429f35cd65f96631b2e136f4f4
Семен <semen_s84@mail.ru>
2020-02-17 10:20:40 +0500

    Delete config.sh

--- source/taglib-1.11.1/config.sh
+++
@@ -1,4 +0,0 @@
-CFLAGS="-g -O2 $([ $(uname -m) = x86_64 ] && echo -fPIC)" \
-cmake -DCMAKE_INSTALL_PREFIX=/usr/src/tools/taglib-1.11.1 \
-      -DCMAKE_BUILD_TYPE=Release \
-      -DBUILD_SHARED_LIBS=ON
