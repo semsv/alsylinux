a6517d1e361dee2945fbb4b9a3f06be05d6dfe45
Семен <semen_s84@mail.ru>
2020-02-17 13:28:45 +0500

    Update config.sh

--- source/applinux/llvm-9.0.1/config.sh
+++ source/applinux/llvm-9.0.1/config.sh
@@ -16,6 +16,12 @@ tar -xf ../compiler-rt-9.0.1.src.tar.xz -C projects &&
 mv tools/clang-9.0.1.src tools/clang &&
 mv projects/compiler-rt-9.0.1.src projects/compiler-rt &&
 
+#Fix a problem introduced by Glibc-2.31:
+if [ "$(ldd --version | grep '2.31')" != "" ]; then
+sed -e '/ipc_perm, mode/s|^|//|' \
+    -i projects/compiler-rt/lib/sanitizer_common/anitizer_platform_limits_posix.cc
+fi
+
 mkdir -p build &&
 cd       build &&
 
