6db290d47b7d67bf3aa6b670730c75b3afed82c7
Семен <semen_s84@mail.ru>
2020-02-19 16:24:47 +0500

    Delete config.sh

--- source/a52dec-0.7.4/config.sh
+++
@@ -1,5 +0,0 @@
-   ./configure --prefix=/usr/src/tools/a52dec-0.7.4 \
-               --mandir=/usr/share/man \
-               --enable-shared \
-               --disable-static \
-               CFLAGS="-g -O2 $([ $(uname -m) = x86_64 ] && echo -fPIC)" && make
