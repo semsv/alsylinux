8969a6c65b81a41da5f93f51364ef72fe0950f8f
Семен <semen_s84@mail.ru>
2020-06-05 13:51:24 +0000

    Add files via upload

--- source/applinux/NetworkManager/1.22.6/config.sh
+++ source/applinux/NetworkManager/1.22.6/config.sh
@@ -1,21 +1,23 @@
-
 app="NetworkManager-1.22.6"
 PREFIX="/usr/src/tools/$app"
+appPython="$(ls -l /usr/bin/python3 | cut -d'>' -f2 | cut -d' ' -f2)" 
+
 
 if [ -d ../build ]; then
   rm -rd ../build
 fi
 
+if [ "$appPython" != "" ]; then
 mkdir -p ../build &&
 cp $app.tar.xz ../build &&
 cd    ../build &&
 tar -xf $app.tar.xz &&
 cd $app &&
 sed '/initrd/d' -i src/meson.build &&
-rm -r /usr/bin/python3.6 &&
-ln -s /usr/local/bin/python3.6 /usr/bin/python3.6 &&
-python3.6 -m pip install -U pip &&
-python3.6 -m pip install -U PyGObject &&
+rm /usr/bin/$appPython &&
+ln -s /usr/local/bin/$appPython /usr/bin/$appPython &&
+python3 -m pip install -U pip &&
+python3 -m pip install -U PyGObject &&
 grep -rl '^#!.*python$' | xargs sed -i '1s/python/&3/' &&
   CXXFLAGS+="-O2 -fPIC"            \
   meson --prefix $PREFIX           \
@@ -34,4 +36,5 @@ grep -rl '^#!.*python$' | xargs sed -i '1s/python/&3/' &&
         -Dsystemdsystemunitdir=no  \
         -Dsystemd_journal=false    \
         -Dqt=false                 \
-  ..
+  .. 
+fi
