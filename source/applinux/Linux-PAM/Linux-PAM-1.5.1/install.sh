b118cd69d3c0ac90cbe0b0d26c6fd8e6ea429e23
Семен <semen_s84@mail.ru>
2020-12-31 06:24:20 +0000

    Add files via upload

--- source/applinux/Linux-PAM/Linux-PAM-1.5.1/install.sh
+++ source/applinux/Linux-PAM/Linux-PAM-1.5.1/install.sh
@@ -19,16 +19,36 @@ EOF
 else 
   exit 1
 fi
+
 if [ $? -eq 0 ]; then
   make install
 else 
   exit 1
 fi
+
 if [ $? -eq 0 ]; then
-chmod -v 4755 $1/sbin/unix_chkpwd
+  chmod -v 4755 $1/sbin/unix_chkpwd &&
+  for file in pam pam_misc pamc
+  do
+      mv -v /usr/lib/lib${file}.so.* $1/lib &&
+      ln -sfv ../../lib/$(readlink /usr/lib/lib${file}.so) /usr/lib/lib${file}.so
+  done
 else 
   exit 1
 fi
+
+if [ $? -eq 0 ]; then
+  if [ ! -d $1/include/security ]; then
+    mkdir -p $1/include/security &&
+    for files in $(ls -1 $1/include/ | grep -v security)
+    do
+      cp -r $1/include/$files $1/include/security
+    done
+  fi
+else 
+  exit 1
+fi
+
 if [ $? -eq 0 ]; then
   echo "install success"
   exit 0
